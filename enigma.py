57e7551742865233401d0be26f53fc14dd383c26
kd-gitgub <keith@dewar.cloud>
2026-01-21 18:22:25 +0000

    Pluglead initial

--- enigma.py
+++ enigma.py
@@ -1,17 +1,21 @@
 
 class PlugLead:
     def __init__(self, mapping):
-        # Your code here
-        raise NotImplementedError()
+        # mapping is a 2-character string like "AB"
+        # Store the two connected letters
+        self.char1 = mapping[0]
+        self.char2 = mapping[1]
 
     def encode(self, character):
-        # Your code here
-        raise NotImplementedError()
-
-
-class Plugboard:
-    # Your code here
-    raise NotImplementedError()
+        # If character matches first letter, return second
+        if character == self.char1:
+            return self.char2
+        # If character matches second letter, return first
+        elif character == self.char2:
+            return self.char1
+        # Otherwise return unchanged
+        else:
+            return character
 
 
 # You will need to write more classes, which can be done here or in separate files, you choose.
